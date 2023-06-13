9e71caa2f38ca8e0d3984c24b1acca1a71153e52
Rex Holmes <holmesrex515@gmail.com>
2023-06-13 22:31:17 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./components/Header";
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;

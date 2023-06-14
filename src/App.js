13635cced66a511438d3320d462875021c812a04
Rex Holmes <holmesrex515@gmail.com>
2023-06-14 22:09:41 +0900

    axios added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import Header from "./components/Header";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [movieList, setMovieList] = useState([])
+  const [watchList, setWatchList] = useState([])
+  const [page, setPage] = useState(1)
+
+  const getData = () => {
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+    .then((res) => {
+      console.log(res.data.results)
+      setMovieList(res.data.results)
+    })
+
+    useEffect(() => {
+      getData()
+    }, [page])
+  }
   return (
     <div className="App">
       <Header />
